a244fcf82eaf89bf7eb0c19bdf80e3927c6c22a0
Bryan Keating <ru.bryankeating@gmail.com>
2025-04-15 19:41:09 -0400

    Troubleshooting

---
+++ practicing_pgn_library.py
@@ -0,0 +1,11 @@
+import chess.pgn
+
+pgn = open('Training_1.pgn.txt.')
+game = chess.pgn.read_game(pgn)
+
+board = game.board()
+for move in game.mainline_moves():
+	board.push(move)
+
+print(board)
+print(game.headers["White"], "vs", game.headers["Black"])
